4fc5327f83188c549a9cfae702449cb121140a4b
James Lu <Jy7lu@edu.uwaterloo.ca>
2018-01-27 19:37:48 -0500

    Just Testing Java
    Test button connection to Java

--- lacasadepappi/app/src/main/java/com/example/jameslu/lacasadepappi/MainActivity.java
+++ lacasadepappi/app/src/main/java/com/example/jameslu/lacasadepappi/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.jameslu.lacasadepappi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Timer;
+import java.util.ArrayList;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView myTextView = findViewById(R.id.text);
+        Timer timekeeper = new Timer();
+        ArrayList<Button> buttons = new ArrayList<Button>();
+        LinearLayout buttonContainer = (LinearLayout) findViewById(R.id.buttonContainer);
+
+
+        //button.setOnClickListener(new View.OnClickListener() {
+        //    public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+         //       myTextView.setVisibility(View.VISIBLE);
+         //   }
+        //});
+
+        for (int i = 0; i < 8; i++){
+            Button buttonTest = new Button(this);
+            buttonContainer.addView(buttonTest);
+
+        }
+
     }
+
+
+
+
+
 }
